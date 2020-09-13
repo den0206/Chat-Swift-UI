b1b12eaa1546c3553fac854887a41e28732fb08b
den0206 <mac.pop@mineo.jp>
2020-09-13 21:01:15 +0900

    Initial Commit

---
+++ Chat Swift-UI/Chat_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_Swift_UIApp.swift
+//  Chat Swift-UI
+//
+//  Created by 酒井ゆうき on 2020/09/13.
+//
+
+import SwiftUI
+
+@main
+struct Chat_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
